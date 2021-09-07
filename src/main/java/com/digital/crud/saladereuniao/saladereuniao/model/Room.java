8afbb93aafe4079f6608f8cc5dee7ef4b86d381b
Wesley Rodrigues <wesleysilvarodrigues25@gmail.com>
2021-09-07 16:20:49 -0300

    refactor:model Room

--- src/main/java/com/digital/crud/saladereuniao/saladereuniao/model/Room.java
+++ src/main/java/com/digital/crud/saladereuniao/saladereuniao/model/Room.java
@@ -1,5 +1,10 @@
 package com.digital.crud.saladereuniao.saladereuniao.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.Entity;
 import javax.persistence.Column;
 import javax.persistence.GeneratedValue;
@@ -7,77 +12,29 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-@Entity
 @Table(name = "meetingroom")
+@Entity
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Room {
-
-    private Long id;
-
-    private String name;
-
-    private String date;
-
-    private String startHour;
-
-    private String endHour;
-
-    public Room(){
-
-    }
-
-    public Room(Long id, String name, String date, String startHour, String endHour){
-        this.id = id;
-        this.name = name;
-        this.date = date;
-        this.startHour = startHour;
-        this.endHour = endHour;
-    }
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
+    private Long id;
 
     @Column(name = "name", nullable = false)
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
+    private String name;
 
     @Column(name = "date", nullable = false)
-    public String getDate() {
-        return date;
-    }
-
-    public void setDate(String date) {
-        this.date = date;
-    }
+    private String date;
 
     @Column(name = "startHour", nullable = false)
-    public String getStartHour() {
-        return startHour;
-    }
-
-    public void setStartHour(String startHour) {
-        this.startHour = startHour;
-    }
-
-    public String getEndHour() {
-        return endHour;
-    }
+    private String startHour;
 
     @Column(name = "endHour", nullable = false)
-    public void setEndHour(String endHour) {
-        this.endHour = endHour;
-    }
+    private String endHour;
 
     @Override
     public String toString() {
--- src/main/java/com/digital/crud/saladereuniao/saladereuniao/repository/RoomRepository.java
+++ src/main/java/com/digital/crud/saladereuniao/saladereuniao/repository/RoomRepository.java
@@ -3,9 +3,7 @@ package com.digital.crud.saladereuniao.saladereuniao.repository;
 import com.digital.crud.saladereuniao.saladereuniao.model.Room;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
 @Repository
-@Transactional
 public interface RoomRepository extends JpaRepository<Room, Long> {
 }
