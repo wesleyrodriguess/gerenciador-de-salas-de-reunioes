32310be85fd4cd888160395d21f98aeebf512bb8
Wesley Rodrigues <wesleysilvarodrigues25@gmail.com>
2021-09-07 13:21:23 -0300

    feat:add crud RoomController

--- src/main/java/com/digital/crud/saladereuniao/saladereuniao/controller/RoomController.java
+++ src/main/java/com/digital/crud/saladereuniao/saladereuniao/controller/RoomController.java
@@ -1,29 +1,67 @@
 package com.digital.crud.saladereuniao.saladereuniao.controller;
 
-
-import com.digital.crud.saladereuniao.saladereuniao.exception.ErrorDetails;
 import com.digital.crud.saladereuniao.saladereuniao.exception.ResourceNotFaundException;
-import org.springframework.http.HttpStatus;
+import com.digital.crud.saladereuniao.saladereuniao.model.Room;
+import com.digital.crud.saladereuniao.saladereuniao.repository.RoomRepository;
+import lombok.val;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.Date;
+import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
-@ControllerAdvice
+@RestController @CrossOrigin(origins = "http://localhost:4200")
+@RequestMapping("/api/v1")
 public class RoomController {
 
-    @ExceptionHandler(ResourceNotFaundException.class)
-    public ResponseEntity<?> resourceNotFoundException(ResourceNotFaundException ex, WebRequest request) {
+    @Autowired
+    private RoomRepository roomRepository;
 
-        ErrorDetails errorDetails = new ErrorDetails(new Date(), ex.getMessage(), request.getDescription(false));
-        return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
+    @GetMapping("/rooms")
+    public List<Room> getAllRooms() {
+        return roomRepository.findAll();
     }
 
-    public ResponseEntity<?>globalExceptionHandler(Exception ex, WebRequest request) {
+    @GetMapping("/rooms/id")
+    public ResponseEntity<Room> getRoomById(@PathVariable(value = "id") long roomId)
+        throws ResourceNotFaundException {
+        Room room = roomRepository.findById(roomId)
+                .orElseThrow(()-> new ResourceNotFaundException("Room not found: " + roomId));
+        return ResponseEntity.ok().body(room);
+    }
 
-        ErrorDetails errorDetails = new ErrorDetails(new Date(), ex.getMessage(), request.getDescription(false));
-        return new ResponseEntity<>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
+    @PostMapping("/rooms")
+    public Room createRoom(@Valid @RequestBody Room room){
+        return roomRepository.save(room);
+    }
+
+    @PutMapping(value = "/rooms/{id}")
+    public ResponseEntity<Room> updateRoom(@PathVariable(value = "id") Long roomId,
+                                           @Valid @RequestBody Room roomDetails) throws ResourceNotFaundException {
+        Room room = roomRepository.findById((roomId))
+                .orElseThrow(()-> new ResourceNotFaundException("Room not found for this id:: " + roomId));
+        
+        room.setName(roomDetails.getName());
+        room.setDate(roomDetails.getDate());
+        room.setStartHour(roomDetails.getStartHour());
+        room.setEndHour(roomDetails.getEndHour());
+        
+        final Room updateRoom = roomRepository.save(room);
+        return ResponseEntity.ok(updateRoom);
+    }
+
+    public Map<String, Boolean> deleteRoom(@PathVariable(value = "id") Long roomId)
+        throws ResourceNotFaundException {
+        Room room = roomRepository.findById(roomId)
+                .orElseThrow(()-> new ResourceNotFaundException("Room not found for this id: "+ roomId));
+
+        roomRepository.delete(room);
+        Map<String, Boolean> response = new HashMap<>();
+        response.put("deleted", Boolean.TRUE);
+
+        return response;
     }
 }
